92d9015b4b52dec2347ab73d2621788e1fe39617
liuzhiq779 <1832875619@qq.com>
2019-04-08 21:03:59 +0800

    test

---
+++ save/index.js
@@ -0,0 +1,10 @@
+const  koa = require('koa2')
+const  app = new koa()
+const { normal} = require('./tpl')
+
+
+app.use(async (ctx,next) => {
+    ctx.type = 'text/html; charset=utf-8'
+    ctx.body = normal
+})
+app.listen(4455)
